b5d8479088a658c160e731659076f10e6b4d39a7
jgreene117 <jgreene117@yahoo.com>
2017-07-07 13:59:39 -0400

    friday 7/7

--- save-and-open.js
+++ save-and-open.js
@@ -22,16 +22,38 @@ function destroyClickedElement(event)
     document.body.removeChild(event.target);
 }
  
-function loadFileAsText(event)
+function loadFileAsText()
 {
-    var pre = document.getElementById("editor");
+    var list = document.getElementsByClassName('ace_line');
+    for(var i = list.length - 1; 0 <= i; i--)
+        if(list[i] && list[i].parentElement)
+            list[i].parentElement.removeChild(list[i]);
+    var pre = document.getElementsByClassName('ace_layer ace_text-layer');
+    var loc = pre[0];
+
     var fileToLoad = document.getElementById("fileToLoad").files[0];
     var fileReader = new FileReader();
     fileReader.onload = function() 
     {
         var textFromFileLoaded = fileReader.result;
-//        console.log(textFromFileLoaded);
-        pre.textContent = textFromFileLoaded;
+//      console.log(textFromFileLoaded);
+        var lines = textFromFileLoaded.split(/\r\n|\r|\n/);
+        for (i=0; i < lines.length; i++) {
+            var node = document.createElement("div");
+            node.setAttribute("class", "ace_line");
+            node.setAttribute("style", "height:16px");
+            node.textContent = lines[i];
+            loc.appendChild(node);
+        }
+        var dummy = document.createElement("input");
+        document.body.appendChild(dummy);
+        //$(dummy).css('display', 'none');
+        dummy.setAttribute("id", "dummy_id");
+        document.getElementById("dummy_id").value = textFromFileLoaded;
+        dummy.select();
+        document.execCommand("copy");
+        document.execCommand("paste");
+        document.body.removeChild(dummy);
     };
     fileReader.readAsText(fileToLoad, "utf-8");
     
@@ -42,4 +64,11 @@ function extractedvals() {
     for (var i=0;i<tobeextractedvals.length;i++){
         console.log(tobeextractedvals[i].textcontent);
     }
+}
+
+function InitialLoad(fileName)
+{
+    var pre = document.getElementById("editor");
+    var fileToLoad = document.getElementByID("initFile");
+    
 }
